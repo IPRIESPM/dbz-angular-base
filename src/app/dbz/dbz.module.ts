130b71d51d8648bf6b31ebedc6b400771b1a55a4
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-02 14:52:47 +0200

    feat: :sparkles: add main page components

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 
@@ -9,7 +11,9 @@ import { MainPageComponent } from './pages/main-page.component';
     MainPageComponent
   ],
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    ListComponent,
+    AddCharacterComponent
   ],
   imports: [
     CommonModule,
