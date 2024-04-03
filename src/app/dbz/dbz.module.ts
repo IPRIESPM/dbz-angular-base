ca04d008159f0cff65f408d8db49c9c495cec429
Isaac <105789412+altaskur@users.noreply.github.com>
2024-04-03 09:26:49 +0200

    feat: :sparkles: add obervables for add and remove characters

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
 import { AddCharacterComponent } from './components/add-character/add-character.component';
+
 
 
 
@@ -16,7 +19,7 @@ import { AddCharacterComponent } from './components/add-character/add-character.
     AddCharacterComponent
   ],
   imports: [
-    CommonModule,
+    CommonModule, FormsModule
   ]
 })
 export class DbzModule { }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -12,11 +12,19 @@ export class MainPageComponent {
   public characters: Character[] = [{
     name: 'Krillin',
     power: 1000
-  },{
+  }, {
     name: 'Goku',
     power: 9500
-  },{
+  }, {
     name: "Vegeta",
     power: 7500
   }];
+
+  onNewCharacter(character: Character): void {
+    this.characters.push(character);
+  }
+
+  onDeleteCharacter(index: number) {
+    this.characters.splice(index, 1);
+  }
 }
